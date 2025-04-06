8653734860d1b8a45f1612a69d817f98e1d21118
manoj-m-01 <manoj904378@gmail.com>
2025-04-06 19:38:28 +0530

    tailwind compile fixed

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,7 @@
 /** @type {import('tailwindcss').Config} */
-export default {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+/*eslint-env node*/
+module.exports = {
+  content: ["./src/**/*.{ts,tsx}"],
   theme: {
     extend: {},
   },
